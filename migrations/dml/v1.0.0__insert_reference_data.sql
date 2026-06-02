32f13e898d860751e03e75a7df02c22455b31839
testuser <testuser.@test.com>
2026-06-02 17:04:12 +0530

    deploying changes to dev

--- migrations/dml/v1.0.0__insert_reference_data.sql
+++ migrations/dml/v1.0.0__insert_reference_data.sql
@@ -1,3 +1,3 @@
 -- migrations/dml/v1.0.0__insert_reference_data.sql--
-INSERT INTO <env>_db.PUBLIC.users (username, email)
-VALUES ('system', 'system@company.com');
+INSERT INTO <env>_db.PUBLIC.users_test (username, email)
+VALUES ('system', 'pavan@company.com');
